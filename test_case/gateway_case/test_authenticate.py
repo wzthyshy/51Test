b7fd7e1415cd5e3377338e6345736907a176c7bd
wzthyshy <1640800093@qq.com>
2022-05-08 22:22:10 +0800

    增加allure报告与接口断言

--- test_case/gateway_case/test_authenticate.py
+++ test_case/gateway_case/test_authenticate.py
@@ -1,10 +1,14 @@
 import os
 
 import pytest
+import allure
 
 from api.gateway_api.authenticate import User
+from common import get_log
 
 
+@allure.severity(allure.severity_level.NORMAL)
+@allure.feature("usercontroller模块")
 class Testlogin():
     """
 
@@ -16,10 +20,20 @@ class Testlogin():
     data_path = os.path.join(BASE_PATH, "data/gateway_data/user_data.yml")
     # 参数化的数据
     sign_data = user.load_yaml(data_path)
-    print(sign_data)
+    #
+    # def __init__(self):
+    #     self.token = None
 
+    @allure.story("用例--用户登录")
+    @allure.description("该用例是针对获取用户登录接口的测试")
     @pytest.mark.parametrize(" username, password, rememberMe, code, msg", sign_data["user_data"])
     def test_login(self, username, password, rememberMe, code, msg):
         res = self.user.login_user(username, password, rememberMe)
-        print(res)
+        self.token = res.get('data')['token_id']
+        print(res.get('code'))
+        get_log.log.info("code ==>> 期望结果：{}， 实际结果：【 {} 】".format(code, res.get('code')))
+        assert res.get('code') == code
+        get_log.log.info("msg ==>> 期望结果：{}， 实际结果：【 {} 】".format(msg, res.get('msg')))
+        assert res.get('msg') == msg
+
 
